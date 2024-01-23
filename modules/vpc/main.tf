47ce3156ce943b019c25530fb2f1e1db0347fb14
Jyothsnashrey <jyothsna.shrey@gmail.com>
2024-01-23 14:53:19 -0600

    added terraform code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,9 +8,14 @@ resource    "aws_subnet" "main" {
   count      = length(var.subnets_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.subnets_cidr, count.index )
-  availability_zone = element(var.az, count.index)
+  availability_zone = element(var.az, count.index)  #this created the subnets in the required zones.
 
   tags = {
     Name = "subnet-${count.index}"
   }
+}
+
+resource "aws_vpc_peering_connection" "main" {
+  vpc_id       = aws_vpc.main.id
+  peer_vpc_id  = data.aws_vpc.default.id
 }
