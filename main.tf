a4c96284368aef1984cbdbb3a956ca65a6b93838
Jyothsnashrey <jyothsna.shrey@gmail.com>
2024-01-24 16:16:12 -0600

    added terraform code

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "vpc" {
   source          = "./modules/vpc"
   for_each        = var.vpc
   vpc_cird        = lookup(each.value, "vpc_cidr", null)
-  subnets_cider   = lookup (each.value "subnets_cidr", null)
-  az              = lookup(each.value "az" , null)
+  subnets_cidr   = lookup (each.value, "subnets_cidr", null)
+  az              = lookup(each.value, "az" , null)
   env             = var.env
   project_name    = var.project_name
 }
