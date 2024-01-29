b26294f534de1734b15e0ec1f269868c5d4f4a74
Jyothsnashrey <jyothsna.shrey@gmail.com>
2024-01-29 11:39:40 -0600

    added terraform code

--- main.tf
+++ main.tf
@@ -73,6 +73,7 @@ module "backend" {
   vpc_id            = module.vpc.vpc_id
   vpc_zone_identifier = module.vpc.app_subnets_ids
   parameters          = ["arn:aws:ssm:us-east-1:046694289523:parameter/${var.env}.${var.project_name}.rds.*"]
+  kms                 = var.kms_key_id
 
 }
 
@@ -89,6 +90,7 @@ module "frontend" {
   vpc_id            = module.vpc.vpc_id
   vpc_zone_identifier = module.vpc.web_subnets_ids
   parameters          = []
+  kms                 = var.kms_key_id
 
 }
 
--- modules/app/main.tf
+++ modules/app/main.tf
@@ -51,6 +51,16 @@ resource "aws_launch_template" "main" {
     name = aws_iam_instance_profile.main.name
   }
 }
+  block_device_mappings {
+  device_name = "/dev/sda1"  #diskname from instance in aws
+
+  ebs {
+    volume_size           = 10
+    encrypted             = true
+    kms_key_id            = var.kms
+    delete_on_termination = true
+  }
+}
 
 
 resource "aws_autoscaling_group" "main" {
